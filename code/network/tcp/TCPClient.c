8b2aefaa7a39fc56af53cb36cffcd192a5359473
Jifei Luo <jifei.luo@outlook.com>
2022-12-21 23:35:45 +0800

    TCP

--- code/network/tcp/TCPClient.c
+++ code/network/tcp/TCPClient.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
 
 #define MAXLINE 1024
 #define TRUE 1
@@ -13,6 +14,7 @@
 int main(int argc, char **argv) {
     int sockfd, n, m;
     char line[MAXLINE + 1];
+    char linein[MAXLINE + 1];
     struct sockaddr_in6 servaddr;
     time_t t0 = time(NULL);
     printf("time #: %ld\n", t0);
@@ -34,10 +36,49 @@ int main(int argc, char **argv) {
     if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
         perror("connect error");
 
-    while(fgets(line, MAXLINE, stdin) != NULL) { 
-        send(sockfd, line, strlen(line), 0);
+    pid_t fpid1 = fork();
+    int status1, status2;
+    if(fpid1==0){
+        while((n = read(sockfd, linein, MAXLINE)) > 0) { 
+            linein[n] = 0;
+            if(fputs(linein, stdout) == EOF)
+                perror("fputs error");
+            char endstr[] = "END";
+            char linestr[MAXLINE + 1];
+            strcpy(linestr, linein);
+            linestr[n-1] = 0;
+            if(strcmp(endstr, linestr)==0){
+                break; 
+            }     
+        }      
+        exit(0);
     }
-
+    else{
+        pid_t fpid2 = fork();
+        if(fpid2==0){
+            while(fgets(line, MAXLINE, stdin) != NULL) { 
+                send(sockfd, line, strlen(line), 0);
+                char endstr[] = "END";
+                char linestr[MAXLINE + 1];
+                strcpy(linestr, line);
+                linestr[strlen(line)-1] = 0;
+                if(strcmp(endstr, linestr)==0){
+                    break;
+                }                
+            }
+            exit(0);
+        }
+        else{
+            while(1){
+                if(waitpid(fpid1, &status1, WNOHANG)||waitpid(fpid2, &status2, WNOHANG)){
+                    kill(fpid1, SIGTERM);
+                    kill(fpid2, SIGTERM);
+                    break;
+                }       
+            }
+        }
+    }
+    
     close(sockfd);
     exit(0);
 }
--- code/network/tcp/TCPServer.c
+++ code/network/tcp/TCPServer.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
 
 #define MAXLINE 1024
 #define TRUE 1
@@ -13,6 +14,8 @@
 int main(int argc, char **argv) {
     int sockfd, fd, n, m;
     char line[MAXLINE + 1];
+    char linein[MAXLINE + 1];
+    char buffer[MAXLINE + 1];
     struct sockaddr_in6 servaddr, cliaddr;
     time_t t0 = time(NULL);
     printf("time #: %ld\n", t0);
@@ -32,7 +35,13 @@ int main(int argc, char **argv) {
     if(listen(sockfd, 5) == -1)
         perror("listen error");
 
+    int flag[10] = {0};
+    // for(int i=0; i<atoi(argv[1])-1; i++){
+    //     pid_t usrpid = fork();
+    // }
+
     while(TRUE) {
+        // printf("%d\n",getpid());
         printf("> Waiting clients ...\r\n");
         socklen_t clilen = sizeof(struct sockaddr);
         fd = accept(sockfd, (struct sockaddr*)&cliaddr, &clilen);
@@ -40,10 +49,54 @@ int main(int argc, char **argv) {
             perror("accept error");
         }
         printf("> Accepted.\r\n");
-        while((n = read(fd, line, MAXLINE)) > 0) { 
-            line[n] = 0;
-            if(fputs(line, stdout) == EOF)
-                perror("fputs error");
+
+        pid_t fpid1 = fork();
+        int status1, status2;
+        if (fpid1==0){
+            while((n = read(fd, linein, MAXLINE)) > 0) { 
+                linein[n] = 0;
+                if(fputs(linein, stdout) == EOF)
+                    perror("fputs error");
+                // strcpy(buffer, linein);
+                // for(int i=0;i<10;i++)
+                //     flag[i]=1;
+                // flag[getppid()%10]=0;
+                // for(int i=0;i<10;i++)
+                //     printf("%d",flag[i]);
+                // printf("\n");
+
+                char endstr[] = "END";
+                char linestr[MAXLINE + 1];
+                strcpy(linestr, linein);
+                linestr[n-1] = 0;
+                if(strcmp(endstr, linestr)==0)
+                    break;
+            }
+            exit(0);
+        }        
+        else{
+            pid_t fpid2 = fork();
+            if(fpid2==0){
+                while(fgets(line, MAXLINE, stdin) != NULL) { 
+                    send(fd, line, strlen(line), 0);
+                    char endstr[] = "END";
+                    char linestr[MAXLINE + 1];
+                    strcpy(linestr, line);
+                    linestr[strlen(line)-1] = 0;
+                    if(strcmp(endstr, linestr)==0)
+                        break;               
+                }
+                exit(0);
+            }
+            else{
+                while(1){ 
+                    if(waitpid(fpid1, &status1, WNOHANG)||waitpid(fpid2, &status2, WNOHANG)){
+                        kill(fpid1, SIGTERM);
+                        kill(fpid2, SIGTERM);
+                        break;
+                    }             
+                }       
+            }     
         }
         close(fd);
     }
